a46a28fe3ed4362c23ab32c8685bbfd5c8f073e9
mabuyo <mabuyo@ualberta.ca>
2016-10-15 19:40:36 -0600

    Collection view for Locations set up for 12 locations, 3 columns and 4 rows.

--- FamJamClock/LocationsCollectionViewController.swift
+++ FamJamClock/LocationsCollectionViewController.swift
@@ -31,11 +31,12 @@ class LocationsCollectionViewController: UICollectionViewController {
             "book.jpg",
             "briefcase.jpg",
             "bug.jpg",
-            "cocktail.jpg",
+            "cocktail-glass.jpg",
             "coffee-cup-on-a-plate-black-silhouettes.jpg",
             "fork-and-knife-silhouette.png",
             "home.jpg",
-            "hostpital-building.jpg"
+            "hostpital-building.jpg",
+            "plane.jpg"
         ]
     }
 
@@ -105,4 +106,33 @@ class LocationsCollectionViewController: UICollectionViewController {
     }
     */
 
+}
+
+fileprivate let sectionInsets = UIEdgeInsets(top: 50.0, left: 20.0, bottom: 50.0, right: 20.0)
+fileprivate let itemsPerRow: CGFloat = 3
+
+
+extension LocationsCollectionViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let paddingSpace = sectionInsets.left * (itemsPerRow + 1)
+        let availableWidth = view.frame.width - paddingSpace
+        let widthPerItem = availableWidth / itemsPerRow
+        
+        return CGSize(width: widthPerItem, height: widthPerItem)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        insetForSectionAt section: Int) -> UIEdgeInsets {
+        return sectionInsets
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return sectionInsets.left
+    }
 }
